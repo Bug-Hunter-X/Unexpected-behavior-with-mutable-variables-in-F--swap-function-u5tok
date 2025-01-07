16976e70ae890bee8733d2cbf2127c72ac2ccd06
BugHunter-X <askzenai@gmail.com>
2025-01-07 17:10:07 +0100

    Add bugSolution.fs file

---
+++ bugSolution.fs
@@ -0,0 +1,11 @@
+let mutable x = 10
+let mutable y = 20
+let mutable z = 30
+
+let swap (x:byref<int>) (y:byref<int>) =
+    let temp = x
+    x <- y
+    y <- temp
+
+swap &x &y
+printf "%d %d %d" x y z
